0022937a79aba102f9771b9aeee07a053ac7eb7c
Yildirim Adiguzel <yildirim@xenn.io>
2020-04-22 11:26:24 +0300

    Initial Commit

---
+++ harray-ios-sdk/harray_ios_sdk.h
@@ -0,0 +1,19 @@
+//
+//  harray_ios_sdk.h
+//  harray-ios-sdk
+//
+//  Created by YILDIRIM ADIGÜZEL on 22.04.2020.
+//  Copyright © 2020 xennio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for harray_ios_sdk.
+FOUNDATION_EXPORT double harray_ios_sdkVersionNumber;
+
+//! Project version string for harray_ios_sdk.
+FOUNDATION_EXPORT const unsigned char harray_ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <harray_ios_sdk/PublicHeader.h>
+
+
